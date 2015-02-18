245812880ceb9768e8191851d6d55266e7295aa0
Daniel Andrus <danieleandrus@gmail.com>
2015-02-18 11:07:13 -0700

    Added filter functionality
    + Added function that applies a filter mask to an image
    + Added 3x3 filter smoothing functionality

--- DanProcessor.cpp
+++ DanProcessor.cpp
@@ -14,6 +14,90 @@
 #include "DanProcessor.h"
 
 
+bool DanProcessor::filter(Image& image, int** mask, int mask_w, int mask_h)
+{
+  // Make sure image isn't null
+  if (image.IsNull()) return false;
+  
+  // Initialize variables
+  Image copy;                           // Copy of original image
+  int img_w;                            // Overal image width
+  int img_h;                            // Overal image height
+  int mask_sum;                         // Sum of numbers in mask
+  int sum[3];                           // Sum of all colors
+  int center_x;                         // Center of mask
+  int center_y;                         // Center of mask
+  int temp, i, j, k, l, x, y;           // Temporary variables
+  
+  // Copy image due to nature of algorithm
+  copy = image;
+  
+  // Get image dimensions
+  img_w = image.Width();
+  img_h = image.Height();
+  
+  // Calculate mask total
+  mask_sum = 0;
+  for (i = 0; i < mask_h; ++i)
+    for (j = 0; j < mask_w; ++j)
+      mask_sum += mask[i][j];
+  
+  // Find center of mask. If mask is even x even, take top-left of center 4
+  center_x = mask_w / 2 - (1 - mask_w % 2);
+  center_y = mask_h / 2 - (1 - mask_h % 2);
+  
+  // Begin applying mask to image
+  for (i = 0; i < img_h; ++i)           // Loop over rows
+  {
+    for (j = 0; j < img_w; ++j)         // Loop over columns
+    {
+      // Reset variables
+      sum[0] = 0;
+      sum[1] = 0;
+      sum[2] = 0;
+      
+      // Center mask over pixel and take weighted average
+      for (k = 0; k < mask_h; ++k)      // Loop over mask rows
+      {
+        for (l = 0; l < mask_w; ++l)    // Loop over mask columns
+        {
+          // Temporarily store variable of current pixel to sum
+          x = l + (l - center_x);
+          y = k + (k - center_y);
+          
+          // If a pixel would be out of bounds, use nearest valid pixel
+          if (x < 0)      x = 0;
+          if (x >= img_w) x = img_w - 1;
+          if (y < 0)      y = 0;
+          if (y >= img_h) y = img_h - 1;
+          
+          // Add RGB values to sum with weights
+          sum[0] = copy[y][x].Red() * mask[k][l];
+          sum[1] = copy[y][x].Green() * mask[k][l];
+          sum[2] = copy[y][x].Blue() * mask[k][l];
+        }
+      }
+      
+      // Average out the sum, truncating decimals
+      sum[0] /= mask_sum;
+      sum[1] /= mask_sum;
+      sum[2] /= mask_sum;
+      
+      // Clip values should they be invalid
+      for (k = 0; k < 3; ++k)
+      {
+        if (sum[k] < 0)     sum[k] = 0;
+        if (sum[k] >= 256)  sum[k] = 256-1; // Why not 255? to match lines 69-72
+      }
+      
+      // Put new RGB values into image
+      image[i][j].SetRGB(sum[0], sum[1], sum[2]);
+    }
+  }
+  
+  return true;
+}
+
 /***************************************************************************//**
  * Menu_DanFunctions_Negate
  * Author - Dan Andrus
@@ -26,30 +110,17 @@
  * Returns
  *          true if successful, false if not
  ******************************************************************************/
-bool DanProcessor::Menu_DanFunctions_Negate(Image& image)
+bool DanProcessor::Menu_Filters_3x3SmoothingFilter(Image& image)
 {
-  if (image.IsNull())
-    return false;
+  // Make sure image isn't null
+  if (image.IsNull()) return false;
   
-  int rows = image.Height();
-  int cols = image.Width();
+  // Copy existing image due to nature of operation
+  int mask[3][3] = {
+    {1, 2, 1},
+    {2, 4, 2},
+    {1, 2, 1}
+  };
   
-  unsigned char lutable[256] = {0};
-  
-  // Build lookup table
-  for (int i = 0; i < 256; i++)
-    lutable[i] = 255-i;
-  
-  for (int i = 0; i < rows; i++)
-  {
-    for (int j = 0; j < cols; j++)
-    {
-      // Negate each pixel
-      image[i][j].SetRGB((lutable[image[i][j].Red()]),
-                         (lutable[image[i][j].Green()]),
-                         (lutable[image[i][j].Blue()]));
-    }
-  }
-  
-  return true;
+  return filter(image, mask, 3, 3);
 }
